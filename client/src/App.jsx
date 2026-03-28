c4b53bfd8db4fbaa28e489f93c193606610eec33
thaiphong89 <phongho08092004@gmail.com>
2026-03-28 13:16:24 +0700

    ui: message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import Message from "./pages/Message";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         {/* Các route khác sẽ thêm vào đây sau */}
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/messages" element={<Message />} />
       </Routes>
     </BrowserRouter>
   );
